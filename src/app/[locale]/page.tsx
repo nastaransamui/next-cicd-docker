92e063742196304d016cb27ed3a067f814ee8008
Majid <nastaran.samui@gmail.com>
2024-08-16 22:14:12 +0700

    TourSection init

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,11 @@
 import Herobaner from '@/components/homeComponents/herobaner';
+import TourSection from '@/components/homeComponents/TourSection';
 
 export default function Home() {
-  return <Herobaner />;
+  return (
+    <>
+      <Herobaner />
+      <TourSection />
+    </>
+  );
 }
---
+++ src/components/homeComponents/TourSection.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TourSection() {
+  return <div>TourSection</div>;
+}
